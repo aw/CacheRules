537667a717e9ee75584dbefdbddda12f805ca59d
Alex Williams <awilliams@alexwilliams.ca>
2016-11-01 07:26:41 +0000

    Update LICENSE year

--- README.md
+++ README.md
@@ -121,4 +121,4 @@ This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at [http://mozilla.org/MPL/2.0/](http://mozilla.org/MPL/2.0/).
 
-Copyright (c) 2014-2015 Alexander Williams, Unscramble
+Copyright (c) 2014-2016 Alexander Williams, Unscramble
--- lib/cache_rules.rb
+++ lib/cache_rules.rb
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Copyright (c) 2014-2015 Alexander Williams, Unscramble <license@unscramble.jp>
+# Copyright (c) 2014-2016 Alexander Williams, Unscramble <license@unscramble.jp>
 #
 # Original Source:  https://github.com/aw/CacheRules
 #
